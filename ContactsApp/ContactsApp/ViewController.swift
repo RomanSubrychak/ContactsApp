69ca6c8a87cf5cab6ac1bdc3b06f7234e4acfcbe
Roman Subrychak <rsubrichak@yahoo.com>
2017-02-28 23:50:54 -0600

    Show Contacts list in a table -Add TableViewController to storyboard -Create named Sections -Create Rows

--- ContactsApp/ContactsApp/ViewController.swift
+++ ContactsApp/ContactsApp/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+	
+	//MARK: - Properties
+	var names = [String: [String]]() {
+		didSet {
+			if names.count != sectionNames.count {
+				sectionNames =  names.keys.sorted()
+			}
+		}
+	}
+	var sectionNames = [String]()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		names.updateValue(["Allen", "Adams"], forKey: "A")
+		names.updateValue(["Cartner", "Cormen","Camel"], forKey: "C")
+		
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
+	
+	//MARK: - Table View
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return sectionNames.count
+	}
+	
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return sectionNames[section]
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if let contacts = names[sectionNames[section]] {
+			return contacts.count
+		}
+		return 0
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath)
+		cell.textLabel?.text = (names[sectionNames[indexPath.section]]!)[indexPath.row]
+		return cell
+	}
+	
 
 }
 
